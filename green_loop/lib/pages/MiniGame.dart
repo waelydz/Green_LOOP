86de2320b4e2278616887277d6eb74492a9beb95
SarahAL09 <saraaltini2001@gmail.com>
2025-04-30 03:31:28 +0400

    final final snake game

--- green_loop/lib/pages/MiniGame.dart
+++ green_loop/lib/pages/MiniGame.dart
@@ -1,127 +1,3 @@
-/*import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:math';
-
-void main () => runApp(MyApp());
-class  MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, 
-      home : landing(),
-    );
-    
-  }
-}
-class landing extends StatefulWidget{
-
-  @override
-  //List<int> snakePosition = [45, 65, 85]; 
-  _HomePageState createState() => _HomePageState();
-
-}
-
-class _HomePageState extends State<HomePage> {
-  static List<int> snakePosition = [45, 65, 85, 105, 125];
-  int numberOfSquares = 760;
-
-  static var randomNumber = Random();
-  int food = randomNumber.nextInt(700);
-  void generateNewFood();
-  food = randomNumber.nextInt(700);
-}
-
-void startGame() {
-  List<int> snakePosition = [45, 65, 85, 105, 125];
-  const duration = const Duration(millieseconds: 300);
-  Timer.periodic(duration, (Timer timer){
-    updateSnake();
-    //if (gameOver()) {
-    // timer.cancel();
-    //_showGameOverScreen();
-    //}
-  });
-}
-
-var direction = 'down';
-void updateSnake() {
-  setState(() {
-    switch (direction) {
-      case 'down':
-      if (snakePosition.last > 740) {
-        snakePosition.add(snakePosition.last + 20 - 760);
-
-      } else {
-        snakePosition.add(snakePosition.last + 20);
-      }
-
-      break;
-
-      case 'up':
-      if (snakePosition.last < 20){
-        snakePosition.add(snakePosition.last - 20 + 760);
-      } else {
-        snakePosition.add(snakePosition.last - 20);
-      }
-
-      break;
-
-      case 'left':
-      if(snakePosition.last % 20 == 0 ){
-        snakePosition.add(snakePosition.last - 1 + 20);
-      } else {
-        snakePosition.add(snakePosition.last - 1);
-      }
-
-      break;
-
-      case 'right' :
-      if ( (snakePosition.last +1) % 20 == 0){
-        snakePosition.add(snakePosition.last + 1 - 20);
-      } else {
-        snakePosition.add(snakePosition.last + 1);
-      }
-      break;
-      default:
-    }
-    if (snakePosition.last == food){
-      generateNewFood();
-    } else {
-      snakePosition.removeAt(0);
-    }
-  });
-}
-
-
-/* 
-bool GameOver() {
-for (int i = 0; i < snakePosition.length; i++){
-int count = 0;
-for (int j = 0; j < snakePosition.length; j++) {
-  if (snakePosition[i] == snakePosition[j]) {
-  count += 1;
-  }
-  if (count == 2){
-  return true;
-  }
-  }
-}
-return false;
-} */
-
-
-/*
-void _showGameOverScreen(){
-showDialog(context: context,
-builder: (BuildContext context){
-return AlertDialog})
-} */
-
-*/
-
-
-
-///////NEW VERSION
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:math';
@@ -150,28 +26,35 @@ class HomePageState extends State<Landing> {
   int food = 300;
   String direction = 'down';
   Timer? timer;
-
+void resetGame() {
+  timer?.cancel();
+  snakePosition = [45, 65, 85, 105, 125];
+  direction = 'down';
+  generateNewFood();
+  startGame();
+  setState(() {});
+}
   @override
   void initState() {
     super.initState();
     generateNewFood();
     startGame();
   }
-
-  void generateNewFood() {
-    setState(() {
-      food = randomNumber.nextInt(numberOfSquares);
-    });
-  }
+  
 
   void startGame() {
     timer = Timer.periodic(Duration(milliseconds: 300), (Timer timer) {
       updateSnake();
     });
   }
-
+  void generateNewFood(){
+    food = randomNumber.nextInt(numberOfSquares);
+  }
   
   void updateSnake() {
+
+    //showGameOverDialog(context);
+
     setState(() {
       switch (direction) {
         case 'down':
@@ -212,19 +95,104 @@ class HomePageState extends State<Landing> {
       } else {
         snakePosition.removeAt(0);
       }
+    //});
+    if(snakePosition.sublist(0, snakePosition.length - 1).contains(snakePosition.last)){
+      timer?.cancel();
+      showGameOverDialog();
+    }
     });
   }
+    void showGameOverDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Game Over'),
+          content: Text('You crashed into yourself!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                resetGame();
+              },
+              child: Text('Restart'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'Snake Game UI Coming Soon...',
-          style: TextStyle(color: Colors.white),
+            body: Column(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                var direction;
+                if (details.delta.dy > 0 && direction != 'up') {
+                  direction = 'down';
+                } else if (details.delta.dy < 0 && direction != 'down') {
+                  direction = 'up';
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (details.delta.dx > 0 && direction != 'left') {
+                  direction = 'right';
+                } else if (details.delta.dx < 0 && direction != 'right') {
+                  direction = 'left';
+                }
+              },
+              child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: numberOfSquares,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 20,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  if (snakePosition.contains(index)) {
+                    return Container(
+                      margin: EdgeInsets.all(1),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    );
+                  } else if (index == food) {
+                    return Container(
+                      margin: EdgeInsets.all(1),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                    );
+                  } else {
+                    return Container(
+                      margin: EdgeInsets.all(1),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[900],
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Swipe to move',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
         ),
-      ),
+     
     );
   }
 }
